28e811c540a546bf7693de355223e297105fb73f
Ale Prest@ <alejandro.modernizacion@gmail.com>
2025-12-09 23:54:17 +0000

    Reformateo con black y agrego IP pública a ALLOWED_HOSTS

--- prestaLabs/settings.py
+++ prestaLabs/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS",
-    default="localhost,127.0.0.1",
-    cast=lambda v: [s.strip() for s in v.split(",")],
-)
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "85.31.224.145"]
 
 
 # Application definition
